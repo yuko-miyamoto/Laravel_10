07bd0c3bde8e022a22c0dde63b2470eb16d4a89f
yuko_miyamoto <vaoovao@gmail.com>
2021-10-12 13:18:35 +0000

    sample2

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -9,7 +9,7 @@
            <div class="container">
                <div class="row">
                    <div class="col-md-8 mx-auto">
-                       <h2>ニュース新規作成</h2>
+                       <h2>ニュース新規作成2</h2>
                    </div>
                </div>
            </div>
