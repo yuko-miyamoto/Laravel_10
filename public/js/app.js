5aa62c51586bc996054cd19faa4fdb973a576795
yuko_miyamoto <vaoovao@gmail.com>
2021-10-14 14:33:08 +0000

    Larave_11課1

--- public/js/app.js
+++ public/js/app.js
@@ -49821,16 +49821,28 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/profile.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/profile.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/admin.scss ***!
-  \*****************************************************************************************/
+/*!***********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/admin.scss ./resources/sass/profile.scss ***!
+  \***********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/js/app.js */"./resources/js/app.js");
 __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/admin.scss */"./resources/sass/admin.scss");
+module.exports = __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/profile.scss */"./resources/sass/profile.scss");
 
 
 /***/ })
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,29 +1,17 @@
-<!DOCTYPE>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>MyProfile</title>
-    </head>
-    <body>
-        {{-- layouts/profile.blade.phpを読み込む --}}
-        @extends('layouts.profile')
-        
-        
-        {{-- profile.blade.phpの@yield('title')に'プロフィール作成画面'を埋め込む --}}
-        @section('title', 'プロフィール作成画面')
-        
-        {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
-        @section('content')
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 mx-auto">
-                        <h2>Myプロフィール作成画面</h2>
-                    </div>
-                </div>
+   {{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- create.blade.phpの@yield('title')に'プロフィール作成画面'を埋め込む --}}
+@section('title', 'プロフィール作成画面')
+
+{{-- create.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
             </div>
-        @endsction
-   
-    </body>
-</html>
+        </div>
+    </div>
+@endsection
