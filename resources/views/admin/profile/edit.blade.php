847af4c99e980d13b4532c29471c60726b79531d
yuko_miyamoto <vaoovao@gmail.com>
2021-10-15 12:01:29 +0000

    Laravel_12課題

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>MyProfile2</title>
-    </head>
-    <body>
-        <h1>Myプロフィール編集画面</h1>
-  
-    </body>
-</html>
+  {{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- create.blade.phpの@yield('title')に'プロフィール編集画面'を埋め込む --}}
+@section('title', 'プロフィール編集画面')
+
+{{-- create.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール編集画面</h2>
+            </div>
+        </div>
+    </div>
+@endsection<!DOCTYPE>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -41,11 +41,35 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
-
                         </ul>
 
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
+                            
+                        {{-- 以下を追記(Laravel_12) --}}
+                        <!-- Authentication Links -->
+                        {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                        @guest
+                        <li><a class="nav-link" herf="{{ route('login') }}">{{ __('Login') }}</a></li>
+                        {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="botton" date-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementByID('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                                
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
+                    {{-- 以上までを追記(Laravel_12) --}}
                         </ul>
                     </div>
                 </div>
