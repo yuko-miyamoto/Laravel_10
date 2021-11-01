f3e0a6a861af1edd08b4e2bc9fd36915b6997f31
yuko_miyamoto <vaoovao@gmail.com>
2021-11-01 11:27:51 +0000

    heroku

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,11 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
     
-    Route::get('/', 'NewsController@index');
-    
     Route::get('/profile', 'ProfileController@index');
     
 });
+
+ Route::get('/', 'NewsController@index');
 
 Auth::routes();
 
